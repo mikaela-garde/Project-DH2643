6a702daaa5f293605e616efa8af590948167d6ff
Mikaela Gärde <51963556+mikaela-garde@users.noreply.github.com>
2022-10-03 11:13:09 +0200

    

---
+++ client/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const App = () => {
+    return <div><p>Experince Saver</p></div>
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);

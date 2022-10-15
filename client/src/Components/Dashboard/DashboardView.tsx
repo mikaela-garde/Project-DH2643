f1649502b098ea2dfbd8d82d01f08ce345b2e84d
John Brink <john-brink@hotmail.com>
2022-10-15 13:01:36 +0200

    gitignored firebase

--- client/src/Components/Dashboard/DashboardView.tsx
+++ client/src/Components/Dashboard/DashboardView.tsx
@@ -3,12 +3,22 @@ import styled from "styled-components";
 import { PrimaryBtn } from '../../StyledComponents';
 import GridView from '../Grid/GridView';
 import HeaderPresenter from '../Header/HeaderPresenter';
+import BackgroundBlobLeftSVG from "../../Images/BackgroundBlobDashboardLeft.svg";
+import BackgroundBlobRightSVG from "../../Images/BackgroundBlobDashboardRight.svg";
 
 const DashboardView= ({}) =>
     <Container>
         <HeaderPresenter NavTitle={"Dashboard"}/>
         <GridView/>
         <CreateExpButton>Create Experience</CreateExpButton>
+        <BackgroundBlobContainerLeft>
+            
+            <BackgroundBlob src ={BackgroundBlobLeftSVG}></BackgroundBlob>
+        </BackgroundBlobContainerLeft>
+        <BackgroundBlobContainerRight>
+            <BackgroundBlob src = {BackgroundBlobRightSVG}></BackgroundBlob>
+        </BackgroundBlobContainerRight>
+
     </Container>
        
 ;
@@ -27,5 +37,23 @@ const Container = styled.div`
     flex-direction: column;
     justify-content: center;
 `
+
+const BackgroundBlobContainerLeft = styled.div`
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: -50px;
+    z-index:-1;
+`;
+
+const BackgroundBlobContainerRight = styled.div`
+    position: absolute;
+
+    right: 0%;
+    z-index: -1;
+`;
+const BackgroundBlob = styled.img`
+    vertical-align: middle;
+`;
 
 export default DashboardView;

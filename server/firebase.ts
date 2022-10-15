25e34ce0abaf94e9ac37a248af7187e981d8fffa
John Brink <john-brink@hotmail.com>
2022-10-15 13:05:52 +0200

    small import changes

---
+++ server/firebase.ts
@@ -0,0 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { Database, getDatabase} from "firebase/database";
+import firebaseConfig from './firebase-config';
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db:Database = getDatabase(firebaseApp);
+
+export default db;

be6353402fd6acc58de8411129cf5b20bbb70407
Jessie Liu <50206187+jessieljx@users.noreply.github.com>
2022-10-23 17:19:16 +0200

    ExpBoardGrid style & FileUploader Signup

--- client/src/Components/Signup/SignupPresenter.ts
+++ client/src/Components/Signup/SignupPresenter.ts
@@ -13,7 +13,14 @@ function SignUpPresenter (props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [image, setImage] = useState((data) => data? data : EmptyProfileImage);
+    const fileTypes = ["JPG", "PNG", "JPEG"];
+    const [file, setFile] = useState(new Image);
+    const [fileName, setFileName] = useState("");
+    const [fileError, setFileError] = useState("");
 
+    const handleFileChange = (file) => {
+        console.log("HALLOOOJJ " + file);
+    };
     useEffect(() => {
         return UserModel.setSignErrorMsg(""); // cleans up error message if user comes back
     }, []);
@@ -29,7 +36,9 @@ function SignUpPresenter (props) {
             if (UserModel.regExSignUp(firstName, lastName)) {
                 UserModel.createNewUserFB(firstName, lastName, email, password == "" ? " ": password, image)
             }
-        }
+        }, 
+        fileTypes: fileTypes,
+        handleFileChange: handleFileChange
     })
 }
 
